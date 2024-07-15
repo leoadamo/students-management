3599d184636b80dcbb113bd2b2b97181e932fbca
Nathalia Monte <nathalia.monte@melhorenvio.com>
2024-07-15 18:15:56 -0300

    feat: configure default client port

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -17,6 +17,10 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
+  server: {
+    port: 3010
+  },
+
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
