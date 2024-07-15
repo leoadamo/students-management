4e324392633048a15c873f5ff8bf13f999e3d737
Nathalia Monte <nathalia.monte@melhorenvio.com>
2024-07-15 16:24:12 -0300

    feat: initial commit client

---
+++ client/layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app dark>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>Meu Projeto</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container fluid>
+        <nuxt />
+      </v-container>
+    </v-main>
+
+    <v-footer app color="primary" dark>
+      <v-col class="text-center">
+        &copy; {{ new Date().getFullYear() }}
+      </v-col>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
---
+++ client/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-text-field v-model="msg" />
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
